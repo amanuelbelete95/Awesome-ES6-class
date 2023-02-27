7a273de49cbdd77eebed660b5623abf90b9af67c
Amanuel Belete <amanuelbelete95@gmail.com>
2023-02-27 17:20:56 +0300

    Fix the linter error

--- modules/library.js
+++ modules/library.js
@@ -3,15 +3,14 @@ export default class Library {
     this.books = [];
   }
 
-  addBook = book => this.books.push(book);
-  
+  addBook = (book) => this.books.push(book);
 
-  removeBook = book => {
+  removeBook = (book) => {
     const index = this.books.indexOf(book);
     if (index !== -1) {
       this.books.splice(index, 1);
     }
   }
-  
-  getBooks = ()=> this.books;
+
+  getBooks = () => this.books;
 }
