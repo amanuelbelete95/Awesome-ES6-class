daaadfc730a7ae777b93f5bc928556c5cf3e053f
Amanuel Belete <amanuelbelete95@gmail.com>
2023-02-27 14:23:55 +0300

    Add the script file using es6 modules

---
+++ index.js
@@ -0,0 +1,22 @@
+
+import time from './modules/time.js'
+
+import loadLibraryFromLocalStorage from './modules/onload.js';
+
+import { listClick,handleAddClick,handleContactClick } from './modules/navigator.js'
+
+import { add, list, add1, contact } from './modules/selector.js'
+
+import populateStorage from './modules/populateStorage.js';
+
+
+list.addEventListener('click', listClick);
+add1.addEventListener('click', handleAddClick);
+contact.addEventListener('click', handleContactClick);
+
+add.addEventListener('click', () => {
+  populateStorage();
+});
+
+loadLibraryFromLocalStorage();
+time();
